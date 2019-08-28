246d028dd6141b6a6fc304b610cd42e1b10615f2
walangkaji <walangkaji@users.noreply.github.com>
2019-08-28 18:54:24 +0700

    Add NULL type to be validate (#4)
    * Add NULL type to be validate
    
    * change NULL to lowercase

--- src/Validator.php
+++ src/Validator.php
@@ -27,6 +27,7 @@ class Validator
             'double' => 'is_double',
             'boolean' => 'is_bool',
             'string' => 'is_string',
+            'null' => 'is_null',
             'number' => [$this, 'isNumber'],
             'array' =>  [$this, 'isArray'],
             'object' =>  [$this, 'isObject'],
